6bd223d14ea8323c0b1bb39ad970692fdba366c8
Dan Alvarez <danalvarez5280@gmail.com>
2018-03-01 11:22:35 -0700

    first commit

---
+++ Block.js
@@ -0,0 +1,29 @@
+class Block {
+    constructor(alive = false, coords) {
+        this.alive = alive;
+        this.coords = coords;
+        this.neighbors = false;
+        this.amountONeighbors;
+        this.willLive; 
+    }
+
+    checkAlive() {
+        if(this.alive) {
+            ///check live conditions
+        }
+        else {
+            // check dead conditions
+        }
+    }
+
+
+}
+
+
+
+module.exports = Block;
+
+//Any live cell with fewer than two live neighbours dies(underpopulation)
+//Any live cell with two or three live neighbours lives on to the next generation
+//Any live cell with more than three live neighbours dies(overpopulation)
+//Any dead cell with exactly three live neighbours becomes a live cell(reproduction)
---
+++ tests/Block-test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+var Block = require('../Block');
+
+describe('Unicorn', function () {
+
+    it('should be a function', function () {
+        assert.isFunction(Block);
+    });
+});
